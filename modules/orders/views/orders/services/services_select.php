982f248e84346d29581f80e643bcad5bb6ec62f9
Gorchel <kasatka567@gmail.com>
2020-12-08 12:46:39 +0300

    add all services count

--- modules/orders/views/orders/services/services_select.php
+++ modules/orders/views/orders/services/services_select.php
@@ -11,27 +11,21 @@
 
     <?php
         $services = array_map(function($service) {
-            return ['label' => $service['count'].' '.$service['name'], 'url' => [UrlHelper::getPathWithParams(['service_id' => $service['id']])]];
+            return [
+                'label' => $service['name'],
+                'template' => '<a href="{url}" data-nav-section="about"><span class="label-id">'.$service['count'].'</span>&nbsp{label}</a>',
+                'url' => [UrlHelper::getPathWithParams(['service_id' => $service['id']])]
+            ];
         }, $servicesList);
 
-        array_unshift($services, ['label' => 'All', 'url' => [UrlHelper::getPathWithParams(['service_id' => '0'])]]);
+        array_unshift($services, [
+                'label' => 'All ('.array_sum(array_column($servicesList, 'count')).')',
+                'url' => [UrlHelper::getPathWithParams(['service_id' => '0'])]
+        ]);
 
         echo Menu::widget([
             'options' => ['class' => 'dropdown-menu', 'aria-labelledby' => "dropdownMenu1"],
             'items' => $services,
         ]);
     ?>
-
-<!--    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">-->
-<!--        <li class="active"><a href="--><!--">All (--><!--)</a></li>-->
-<!---->
-<!--        -->
-<!--        <li><a href=""><span class="label-id">214</span>  Real Views</a></li>-->
-<!--        <li><a href=""><span class="label-id">215</span> Page Likes</a></li>-->
-<!--        <li><a href=""><span class="label-id">10</span> Page Likes</a></li>-->
-<!--        <li><a href=""><span class="label-id">217</span> Page Likes</a></li>-->
-<!--        <li><a href=""><span class="label-id">221</span> Followers</a></li>-->
-<!--        <li><a href=""><span class="label-id">224</span> Groups Join</a></li>-->
-<!--        <li><a href=""><span class="label-id">230</span> Website Likes</a></li>-->
-<!--    </ul>-->
 </div>
